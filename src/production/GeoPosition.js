ce3bd359c66642d1de82f405355801afda8815d1
Janitha Jayaweera <janitha_j@hotmail.com>
2012-03-13 21:04:25 -0700

    deleted comments

--- src/production/GeoPosition.js
+++ src/production/GeoPosition.js
@@ -1,7 +1,3 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 var mygps = {}; 
 mygps.GeoPosition = function(latitude,longitude,bearing,altitude,altitudeAngle){
     this.latitude = latitude;
