423cd3180cb15b90aa207c01e2809a804ae949bc
Janitha Jayaweera <janitha_j@hotmail.com>
2012-03-13 21:09:55 -0700

    delete comments

--- test/test/TestDriver.js
+++ test/test/TestDriver.js
@@ -1,84 +1,5 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-//GreeterTest = TestCase("GreeterTest");
-//GreeterTest.prototype.testGreet = function() {
-//    var greeter = new myapp.Greeter();
-//    assertEquals("Hello World!", greeter.greet("World"));
-//};
-//
-///*
-// * I want to draw line in 3D
-// * I need to know the start
-// * I need to know the end
-// * I need to know the gradient
-// * I want to draw some boxes in 3D
-// */
-//
-//DoNothingTest = TestCase("DoNothingTest");
-//DoNothingTest.prototype.testDoNothing = function(){
-//    assertEquals(0,0);
-//}
-//
 var httpServer = new myhardware.Server();
-//var qServer = new myapp.Server();
-//LineTest = TestCase("getStartLatitude");
-//LineTest.prototype.testGetLatitude = function (){
-//    var DataFromServer = new myapp.DataFromServer(httpServer);
-//    httpServer.setLatitude(0.0);
-//    assertEquals (0,DataFromServer.getLatitude());
-//}
-//
-//LineTest = TestCase("getStartLatitude10");
-//LineTest.prototype.testGetLatitude = function (){
-//    var DataFromServer = new myapp.DataFromServer(httpServer);
-//    httpServer.setLatitude(1.0);
-//    assertEquals (1.0,DataFromServer.getLatitude());
-//}
-//
-//LineTest = TestCase("getStartLatitude20");
-//LineTest.prototype.testGetLatitude = function (){
-//    var DataFromServer = new myapp.DataFromServer(httpServer);
-//    httpServer.setLatitude(2.0);
-//    assertEquals (2.0,DataFromServer.getLatitude());
-//}
-//
-//LineTest = TestCase("getStartLongitude0");
-//LineTest.prototype.testGetLongitude = function (){
-//    var DataFromServer = new myapp.DataFromServer(httpServer);
-//    httpServer.setLongitude(0.0);
-//    assertEquals (0.0,DataFromServer.getLongitude());
-//}
-//LineTest = TestCase("getStartLongitude2");
-//LineTest.prototype.testGetLongitude = function (){
-//    var DataFromServer = new myapp.DataFromServer(httpServer);
-//    httpServer.setLongitude(2.0);
-//    assertEquals (2.0,DataFromServer.getLongitude());
-//}
-//LineTest = TestCase("getStartAltitude2");
-//LineTest.prototype.testGetAltitude = function (){
-//    var DataFromServer = new myapp.DataFromServer(httpServer);
-//    httpServer.setLongitude(0.0);
-//    httpServer.setLatitude(0.0);
-//    httpServer.setAltitude(2.0);
-//    assertEquals (2.0,DataFromServer.getAltitude());
-//}
-//LineTest = TestCase("getAngleFromHorizon");
-//LineTest.prototype.testGetAngleFromHorizon = function (){
-//    var DataFromServer = new myapp.DataFromServer(httpServer);
-//    httpServer.setAltitudeAngle(0.0);
-//    assertEquals(0.0,DataFromServer.getAltitudeAngle());
-//}
-//
-//LineTest = TestCase("getAngleFromHorizon2");
-//LineTest.prototype.testGetAngleFromHorizon = function (){
-//    var DataFromServer = new myapp.DataFromServer(httpServer);
-//    httpServer.setAltitudeAngle(2.0);
-//    assertEquals(2.0,DataFromServer.getAltitudeAngle());
-//}
+
 LineTest = TestCase("getLatOnLineAt0_0_0");
 LineTest.prototype.testGetLatOnLineAt0_0_0 = function(){
     var DataFromServer = new myapp.DataFromServer(httpServer);
@@ -92,8 +13,7 @@ LineTest.prototype.testGetLatOnLineAt50_0_0 = function(){
     var DataFromServer = new myapp.DataFromServer(httpServer);
     httpServer.setNextPosition(new mygps.GeoPosition(50*Math.PI/180,0,0,0,0));
     var pos1 = DataFromServer.getNextPosition();
-    //httpServer.setLatitude(50*Math.PI/180);
-    //httpServer.setBearing(0);
+
     assertEquals (50*Math.PI/180, DataFromServer.getLatOnLine(pos1,0));
 }
 LineTest = TestCase("getLatOnLineAt0_0_112");
@@ -101,8 +21,7 @@ LineTest.prototype.testGetLatOnLineAt0_0_112 = function(){
     var DataFromServer = new myapp.DataFromServer(httpServer);
     httpServer.setNextPosition(new mygps.GeoPosition(0,0,0,0,0));
     var pos1 = DataFromServer.getNextPosition();
-    //httpServer.setLatitude(0);
-    //httpServer.setBearing(0);
+
     assertEquals (0.017454064183553972, DataFromServer.getLatOnLine(pos1,111.2*1000));
 }
 //http://www.movable-type.co.uk/scripts/latlong.html
