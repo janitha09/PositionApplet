374fe9a5c96a28e6abb5470c3f4b9dc4bf976149
Janitha Jayaweera <janitha_j@hotmail.com>
2012-03-22 08:49:20 -0700

    data is not persistent on a page refresh in javascript

--- src/production/hardware.js
+++ src/production/hardware.js
@@ -1,21 +1,28 @@
 var myhardware = {};
 myhardware.Server = function(){
-    this.positionQueue =[];
+    this.positionQueue = [];
 };
-myhardware.Server.prototype.getNextPosition = function(){
-    return this.positionQueue.shift();
+myhardware.Server.prototype.getNextPosition = function(index){
+    return this.positionQueue[index];
 }
 myhardware.Server.prototype.setNextPosition = function(pos){
+    if (typeof AndroidJavaInterface=="undefined"){
+        this.positionQueue.push(pos);
+        return;
+    }
     if (pos!=null){
         var positionJSONString = JSON.stringify(pos);
         AndroidJavaInterface.setPosition(positionJSONString);
     }
     var jsonStringPosFromAndroid = AndroidJavaInterface.getPosition();
     var androidPosJsonObject = JSON.parse(jsonStringPosFromAndroid);
-    var anotherpos = new mygps.GeoPosition(androidPosJsonObject.latitude, 
+    var androidPosJsonObjectConvertedToGeoPosition = new mygps.GeoPosition(androidPosJsonObject.latitude, 
         androidPosJsonObject.longitude, androidPosJsonObject.bearing, 
         androidPosJsonObject.altitude, androidPosJsonObject.altitudeAngle);
-    this.positionQueue.push(anotherpos);
+    this.positionQueue.push(androidPosJsonObjectConvertedToGeoPosition);
+}
+myhardware.Server.prototype.getPositionQueueSize = function(){
+    return this.positionQueue.length;
 }
 myhardware.Server.prototype.incrementCounter = function(){
     return AndroidJavaInterface.incrementCounter();
