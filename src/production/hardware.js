348e3377311af79a01876a4f04ab831b921fe134
Janitha Jayaweera <janitha_j@hotmail.com>
2012-03-28 22:45:12 -0700

    Handle the situation where the AndroidJavaInterface is not defined i.e. running the scripts within firefox inorder to use firebug

--- src/production/hardware.js
+++ src/production/hardware.js
@@ -6,17 +6,18 @@ myhardware.Server.prototype.getNextPosition = function(index){
     return this.positionQueue[index];
 }
 myhardware.Server.prototype.setNextPosition = function(pos){
-    if (typeof AndroidJavaInterface=="undefined"){
-        this.positionQueue.push(pos);
+    if (typeof AndroidInterface == "undefined"){
+        this.androidInterfaceIsUndefined(pos);
         return;
     }
-    if (pos!=null){
+    else if (pos!=null){
         var positionJSONString = JSON.stringify(pos);
         AndroidJavaInterface.setPosition(positionJSONString);
     }
     var jsonStringPosFromAndroid = AndroidJavaInterface.getPosition();
     var androidPosJsonObject = JSON.parse(jsonStringPosFromAndroid);
-    var androidPosJsonObjectConvertedToGeoPosition = new mygps.GeoPosition(androidPosJsonObject.latitude, 
+    var androidPosJsonObjectConvertedToGeoPosition = 
+        new mygps.GeoPosition(androidPosJsonObject.latitude, 
         androidPosJsonObject.longitude, androidPosJsonObject.bearing, 
         androidPosJsonObject.altitude, androidPosJsonObject.altitudeAngle);
     this.positionQueue.push(androidPosJsonObjectConvertedToGeoPosition);
@@ -26,4 +27,13 @@ myhardware.Server.prototype.getPositionQueueSize = function(){
 }
 myhardware.Server.prototype.incrementCounter = function(){
     return AndroidJavaInterface.incrementCounter();
+}
+myhardware.Server.prototype.androidInterfaceIsUndefined = function(pos){
+    if (pos==null){
+        return;
+    }
+    else if (pos!=null){
+        this.positionQueue.push(pos);
+        return;
+    }
 }
